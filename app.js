0fea73515ef0e68794a2a916831acad68f3742de
Fernando Pacheco <fpacheco326@gmail.com>
2019-04-06 11:33:39 -0700

    method override

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+var methodOverride = require('method-override')
+
+
+var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
+
+
+	app.use(methodOverride('_method'))
+
+app.use(express.static("public"));
+
+	app.use(bodyParser.urlencoded({ extended: false }))
+	 
+
+	app.use(bodyParser.json())
 
 
 var mysql      = require('mysql');
@@ -11,7 +25,8 @@ var connection = mysql.createConnection({
 });
  
 connection.connect();
- 
+
+
 app.get('/animals.json', function(req, res){
 	connection.query('SELECT * FROM animals', function (error, results, fields) {
 	  if (error) res.send(error)
@@ -19,6 +34,43 @@ app.get('/animals.json', function(req, res){
 	});
 });
 
+
+app.post('/animals-insert', function(req, res){
+	connection.query('INSERT INTO animals (animal_name) VALUES (?)', [req.body.animal_name],function (error, results, fields) {
+	  if (error) res.send(error)
+	  else res.json({
+	  	message: 'success'
+	  });
+	});
+});
+
+app.delete('/animals-delete', function(req, res){
+	connection.query('DELETE FROM animals WHERE id = (?)', [req.body.animal_id],function (error, results, fields) {
+	  
+	  res.redirect('/');
+	
+	});
+});
+
+
+	app.put('/animals-update/:id', function(req, res){
+		connection.query('UPDATE animals SET animal_name = (?) WHERE id = (?)', [req.body.animal_name, req.params.id],function (error, results, fields) {
+		  
+		  res.redirect('/');
+		
+		});
+	});
+
+app.get('*', function(req, res){
+	res.redirect('/')
+});
+
 app.listen(3001, function(){
 	console.log('listening on 3001');
 });
+
+
+
+
+
+
